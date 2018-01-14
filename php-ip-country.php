dcea80bc07684a465ce3e6acaa245116eba60df1
stanma9107 <lol97554@gmail.com>
2018-01-14 17:14:33 +0800

    ip-php

--- php-ip-country.php
+++ php-ip-country.php
@@ -2,7 +2,18 @@
 //判斷地區
     $ch = curl_init();
     $timeout = 5;
-    $ip = $_SERVER['HTTP_HOST'];//取得ip
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))
+{
+  $ip=$_SERVER['HTTP_CLIENT_IP'];
+}
+else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+{
+  $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+}
+else
+{
+  $ip=$_SERVER['REMOTE_ADDR'];
+} //取得使用者ip進行區域判斷
     $url = "http://ip-api.com/php/{$ip}";//傳送ip至ip-api伺服器
     curl_setopt ($ch, CURLOPT_URL, $url);
     curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
