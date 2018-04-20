83163c587a6ee168ac43c3c61fac4ccdd7bca0e6
stanma9107 <lol97554@gmail.com>
2018-04-20 15:34:30 +0800

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -3,9 +3,11 @@
     $dbuser = 'mysql_user';    //使用者名稱
     $dbpass = 'mysql_password';   //使用者密碼
     $dbname = 'mysql_databaseName';   //資料庫名稱
-    $conn = mysql_connect($dbhost, $dbuser, $dbpass) or die('Error with MySQL connection');
+    $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+    if(!$conn){
+        echo 'Failed';
+    }
     mysql_query("SET NAMES 'utf8'");
-    mysql_select_db($dbname);
     $sql = "SELECT COUNT(*) as total FROM `FileList` WHERE `class` = 0;";
     $result = mysql_query($sql) or die('MySQL query error');
     while($row = mysql_fetch_array($result)){
